65a2cfdeb74ed7da9d60c30b0affcd5db41bfd30
Satoshi Matsushita <satoshi.matsushita@necect.com>
2018-06-02 20:30:41 -0700

    Now working with python3.5 and opencv-python-3.4.1.15 	modified:   trans.py

--- trans.py
+++ trans.py
@@ -13,32 +13,31 @@ Options:
 """.format(f=__file__)
 
 import sys
-from PIL import Image
+import cv2
 import numpy as np
 from docopt import docopt
 
-# args = sys.argv
-# print(args)
-# print('arg[1] = '+args[1])
 args = docopt(__doc__)
 
 infile = args['<jpeg_in>']
 # reading original jpeg image
-img  = Image.open(infile)
+org_img  = cv2.imread(infile, cv2.IMREAD_UNCHANGED)
 
-# get width and height of original image
-width, height = img.size
+# reading it in gray scale
+gray_img  = cv2.imread(infile, cv2.IMREAD_GRAYSCALE)
 
-# create Image object in the same size of original
-img2 = Image.new('RGB', (width, height))
+# detect edge
+canny_img = cv2.Canny(gray_img, 50, 110)
 
-img_pixels = []
-for y in range(height):
-    for x in range(width):
-        # (x, y) = (0, 0) is left upper corner
-        # get pixel at (x, y) and append it to img_pixels
-        img_pixels.append(img.getpixel((x, y)))
-# convert img_pixels to nmmpy array for ease of caliculation later
-img_pixels = np.array(img_pixels)
+ORG_WIN = "org"
+GRY_WIN = "gray"
+CAN_WIN = "canny"
 
-img.show()
+cv2.imshow(ORG_WIN, org_img)
+cv2.imshow(GRY_WIN, gray_img)
+cv2.imshow(CAN_WIN, canny_img)
+
+# terminating
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
