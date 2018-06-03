f856d5a11343ff2147a2606ab7ad6e0a8efe6544
Satoshi Matsushita <satoshi.matsushita@necect.com>
2018-06-02 23:21:57 -0700

    Added trackbar but does not work. want to try plt trackbar. plt loops internally. 	modified:   trans.py

--- trans.py
+++ trans.py
@@ -25,53 +25,45 @@ infile = args['<jpeg_in>']
 def doNothing(self):
     pass
 
-win_name = "Trackbar"
-# OpenCVのTrackbar便利クラス
-class CVTrackbar:
-    def __init__(self, param_name, win_name, param_min, param_max, update_func=doNothing):
-        self._update_func = update_func
-        self._trackbar = cv2.createTrackbar(param_name, win_name, param_min, param_max, self._callback)
-        self._param_name = param_name
-        self._win_name = win_name
-
-    # Trackbarの値を参照
-    def value(self):
-        return cv2.getTrackbarPos(self._param_name, self._win_name)
-
-    # Trackbarの値を設定
-    def setValue(self, value):
-        cv2.setTrackbarPos(self._param_name, self._win_name, value)
-
-    def _callback(self, x):
-        self._update_func(x)
-
-    # TrackbarのCallbackを設定
-    def setCallBack(self, update_func):
-        self._update_func = update_func
-
-low_threshold_trackbar = CVTrackbar("LowThr", win_name, 0, 200)
-low_threshold_trackbar.setValue(100)
-high_threshold_trackbar = CVTrackbar("HiThr", win_name, 0, 200)
-high_threshold_trackbar.setValue(100)
+cv2.namedWindow('edge')
+cv2.namedWindow('orig')
+cv2.createTrackbar('LowThs', 'edge', 0, 200, doNothing)
+cv2.createTrackbar('HighThs', 'edge', 0, 300, doNothing)
 
 # reading original jpeg image
 org_img  = cv2.imread(infile, cv2.IMREAD_UNCHANGED)
+cv2.imshow('image', org_img)
 
 # reading it in gray scale
 gray_img  = cv2.imread(infile, cv2.IMREAD_GRAYSCALE)
 
-# detect edge
-# edges_img = cv2.Canny(gray_img, 50, 110)
-edges_img = cv2.Canny(gray_img, low_threshold_trackbar.value(), upper_threshold_trackbar.value())
-
 # plt.subplot(121),plt.imshow(org_img,cmap = 'Accent')
 # plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+# display gray image to edit by plt
 plt.subplot(121),plt.imshow(gray_img,cmap = 'gray')
 plt.title('Gray Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges_img,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-
 plt.show()
+
+low = 100
+high = 200
+while (1):
+    # detect edge
+    # edges_img = cv2.Canny(gray_img, 50, 110)
+    edges_img = cv2.Canny(gray_img, low, high)
+
+    cv2.imshow('edge', edges_img)
+
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    # get threashold value
+    low = cv2.getTrackbarPos('LowThs', 'image')
+    high = cv2.getTrackbarPos('HighThs', 'image')
+
+#    plt.redraw()
+
+
 # terminating
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
