fd93cd80980987f1c83a39f7d720440aa06df85e
Satoshi Matsushita <satoshi.matsushita@necect.com>
2018-06-02 23:45:43 -0700

    Works with PLT window. 	modified:   trans.py

--- trans.py
+++ trans.py
@@ -25,14 +25,9 @@ infile = args['<jpeg_in>']
 def doNothing(self):
     pass
 
-cv2.namedWindow('edge')
-cv2.namedWindow('orig')
-cv2.createTrackbar('LowThs', 'edge', 0, 200, doNothing)
-cv2.createTrackbar('HighThs', 'edge', 0, 300, doNothing)
-
 # reading original jpeg image
-org_img  = cv2.imread(infile, cv2.IMREAD_UNCHANGED)
-cv2.imshow('image', org_img)
+## org_img  = cv2.imread(infile, cv2.IMREAD_UNCHANGED)
+## cv2.imshow('image', org_img)
 
 # reading it in gray scale
 gray_img  = cv2.imread(infile, cv2.IMREAD_GRAYSCALE)
@@ -42,29 +37,18 @@ gray_img  = cv2.imread(infile, cv2.IMREAD_GRAYSCALE)
 # display gray image to edit by plt
 plt.subplot(121),plt.imshow(gray_img,cmap = 'gray')
 plt.title('Gray Image'), plt.xticks([]), plt.yticks([])
-plt.show()
 
+# detect edge
 low = 100
 high = 200
-while (1):
-    # detect edge
-    # edges_img = cv2.Canny(gray_img, 50, 110)
-    edges_img = cv2.Canny(gray_img, low, high)
+edge_img = cv2.Canny(gray_img, low, high)
+plt.subplot(122),plt.imshow(edge_img,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
-    cv2.imshow('edge', edges_img)
-
-    k = cv2.waitKey(1) & 0xFF
-    if k == 27:
-        break
-
-    # get threashold value
-    low = cv2.getTrackbarPos('LowThs', 'image')
-    high = cv2.getTrackbarPos('HighThs', 'image')
-
-#    plt.redraw()
-
+# show
+plt.show()
 
 # terminating
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
